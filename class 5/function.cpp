d8b3b06a684ef56d9027222b82792033ff627f7f
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-25 21:37:11 +0600

    array

--- class 5/function.cpp
+++ class 5/function.cpp
@@ -1,10 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int A[100]={2,5,7};
-// scanf("%d %d %d",&A[0],&[1] &)
-// A[3]=A[0]+A[1]A[2];
-for(int i=0;i<10;i++) scanf("%d",&A[i]);
-for(int i=0;i<5;i++)A[i]+=1;
-for(int i=0;i<10; i++) printf("%d",A[i]);
+ 
+int main()
+{
+    // an array of 10 integers. 
+    // If arr[0] is stored at
+    // address x, then arr[1] is
+    // stored at x + sizeof(int)
+    // arr[2] is stored at x +
+    // sizeof(int) + sizeof(int)
+    // and so on.
+    int arr[5], i;
+ 
+    cout << "Size of integer in this compiler is "
+         << sizeof(int) << "\n";
+ 
+    for (i = 0; i < 5; i++)
+        // The use of '&' before a variable name, yields
+        // address of variable.
+        cout << "Address arr[" << i << "] is " << &arr[i]
+             << "\n";
+ 
+    return 0;
 }
