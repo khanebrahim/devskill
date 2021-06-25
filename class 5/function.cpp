e59a343d8fed5683a2cde42c1a2e962b6cae7d5c
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-25 20:49:15 +0600

    first commit

---
+++ class 5/function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int A[100]={2,5,7};
+// scanf("%d %d %d",&A[0],&[1] &)
+// A[3]=A[0]+A[1]A[2];
+for(int i=0;i<10;i++) scanf("%d",&A[i]);
+for(int i=0;i<5;i++)A[i]+=1;
+for(int i=0;i<10; i++) printf("%d",A[i]);
+}
